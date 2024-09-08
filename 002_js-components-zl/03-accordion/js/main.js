6cd1c2811b86f01fb7fbfd5be21c352c714fc555
chlvk <ochlvk@gmail.com>
2024-09-08 08:35:15 +0300

    add animation

--- 002_js-components-zl/03-accordion/js/main.js
+++ 002_js-components-zl/03-accordion/js/main.js
@@ -3,7 +3,19 @@ const accordionsContainer = document.querySelector('.accordion-container')
 
 accordionsContainer.addEventListener('click', ({ target }) => {
   const accordionHeader = target.closest('.accordion__header')
+  const accordionContent = accordionHeader.nextElementSibling
+  const accordionInner = accordionContent.children[0]
   if (accordionHeader) {
-    accordionHeader.parentElement.classList.toggle('is-open')
+    const accordion = accordionHeader.parentElement
+    let height
+
+    if (accordion.classList.contains('is-open')) {
+      height = 0
+    } else {
+      height = accordionInner.getBoundingClientRect().height
+    }
+
+    accordion.classList.toggle('is-open')
+    accordionContent.style.height = height + 'px'
   }
 })
