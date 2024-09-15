217b4b7325e8451a8a70441df531a537527f98a2
chlvk <ochlvk@gmail.com>
2024-09-15 20:25:51 +0300

    add component

--- 002_js-components-zl/10-datepicker/js/main.js
+++ 002_js-components-zl/10-datepicker/js/main.js
@@ -1,15 +1,27 @@
 const date = new Date(2019, 1, 13)
 const form = document.querySelector('form')
+const input = document.querySelector('input')
 
 const datetimeToDate = (datetime) => {
-  const [year, month] = datetime.split('-')
+  const [year, month, day = 1] = datetime.split('-')
     .map(num => parseInt(num))
 
-  return new Date(year, month - 1)
+  return new Date(year, month - 1, day)
 }
 
 const toTwoDigitString = (number) => {
   return number.toString().padStart(2, '0')
+}
+
+const getCoords = (elem) => {
+  let box = elem.getBoundingClientRect()
+
+  return {
+    top: box.top + window.scrollY,
+    right: box.right + window.scrollX,
+    bottom: box.bottom + window.scrollY,
+    left: box.left + window.scrollX
+  }
 }
 
 const createDateGridHTML = (date) => {
@@ -23,6 +35,7 @@ const createDateGridHTML = (date) => {
 
   for (let day = 1; day <= daysInMonth; day++) {
     const button = document.createElement('button')
+    button.setAttribute('type', 'button')
     if (day === 1) button.style.setProperty('--firstDayOfMonth', firstDayOfMonth + 1)
 
     const datetimeDay = toTwoDigitString(day)
@@ -62,19 +75,19 @@ const createYearMonthIndicatorTimeElement = (date) => {
     `
 }
 
-const createDatepicker = date => {
+const createDatepicker = (input, date) => {
   const datepicker = document.createElement('div')
   datepicker.classList.add('datepicker')
 
   const previousNextMonthButtons = `
     <div class="datepicker__buttons">
-      <button class="datepicker__previous">
+      <button class="datepicker__previous" type="button">
         <svg viewBox="0 0 20 20">
           <path fill="currentColor" d="M7.05 9.293L6.343 10 12 15.657l1.414-1.414L9.172 10l4.242-4.243L12 4.343z" /></svg>
         </svg>
       </button>
 
-      <button class="datepicker__next">
+      <button class="datepicker__next" type="button">
         <svg viewBox="0 0 20 20">
           <path fill="currentColor" d="M12.95 10.707l.707-.707L8 4.343 6.586 5.757 10.828 10l-4.242 4.243L8 15.657l4.95-4.95z" />
         </svg>
@@ -106,6 +119,15 @@ const createDatepicker = date => {
     ${previousNextMonthButtons}
     ${calendar}
   `
+  datepicker.setAttribute('hidden', true)
+  input.addEventListener('click', () => {
+    datepicker.removeAttribute('hidden')
+  })
+  document.addEventListener('click', event => {
+    if (event.target.closest('.datepicker')) return
+    if (event.target.closest('input') === input) return
+    datepicker.setAttribute('hidden', true)
+  })
 
   const previousNextButtons = datepicker.querySelector('.datepicker__buttons')
 
@@ -131,7 +153,31 @@ const createDatepicker = date => {
     dategrid.innerHTML = createDateGridHTML(targetDate)
   })
 
+  const dategrid = datepicker.querySelector('.datepicker__date-grid')
+  dategrid.addEventListener('click', ({ target }) => {
+    if (!target.matches('button')) return
+
+    const buttons = [...target.parentElement.children]
+    buttons.forEach(button => button.classList.remove('is-selected'))
+    target.classList.add('is-selected')
+
+    const time = target.firstElementChild
+    const datetime = time.getAttribute('datetime')
+    const selectedDate = datetimeToDate(datetime)
+    const year = selectedDate.getFullYear()
+    const month = toTwoDigitString(selectedDate.getMonth() + 1)
+    const day = toTwoDigitString(selectedDate.getDate())
+    const formatted = `${day}/${month}/${year}`
+    input.value = formatted
+  })
+
+  const inputCoords = getCoords(input)
+  const oneRem = parseFloat(getComputedStyle(document.body)['font-size'])
+
+  datepicker.style.left = `${inputCoords.left}px`
+  datepicker.style.top = `${inputCoords.bottom + oneRem}px`
+
   return datepicker
 }
 
-form.appendChild(createDatepicker(date))
+form.appendChild(createDatepicker(input, date))
