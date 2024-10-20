06f6ef16b80589a18cf0437ffeccaf54b100e0ef
chlvk <ochlvk@gmail.com>
2024-10-20 14:10:58 +0300

    add component

--- 013_ui-components-mxgrph/03_cart/js/main.js
+++ 013_ui-components-mxgrph/03_cart/js/main.js
@@ -63,3 +63,91 @@
     window.addEventListener('scroll', calcFixedElPosition)
   })()
 
+const productBtnEls = Array.from(document.querySelectorAll('.product__btn'))
+const cartProductsListEl = document.querySelector('.cart-content__list')
+const cartEl = document.querySelector('.cart')
+const cartQuantityEl = document.querySelector('.cart__quantity')
+const fullPriceEl = document.querySelector('.fullprice')
+let price = 0
+
+const getRandomId = () => {
+  return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15)
+}
+
+const getPriceWithoutSpaces = (str) => {
+  return str.replace(/\s/g, '')
+}
+
+const getNormalPrice = (str) => {
+  return String(str).replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 ')
+}
+
+const plusFullPrice = (currentPrice) => {
+  return price += currentPrice
+}
+
+const minusFullPrice = (currentPrice) => {
+  return price -= currentPrice
+}
+
+const printQuantity = () => {
+  const productsListLength = cartProductsListEl.querySelector('.simplebar-content').children.length
+  cartQuantityEl.textContent = productsListLength
+  productsListLength > 0 ? cartEl.classList.add('active') : cartEl.classList.remove('active')
+}
+
+const printFullPrice = () => {
+  fullPriceEl.textContent = `${getNormalPrice(price)} ₽`
+}
+
+const generateCartProduct = (img, title, price, id) => {
+  return `
+    <li class="cart-content__item">
+      <article class="cart-content__product cart-product" data-id=${id}>
+        <img src="${img}" alt="Macbook" class="cart-product__img">
+        <div class="cart-product__text">
+          <h3 class="cart-product__title">${title}</h3>
+          <span class="cart-product__price">${getNormalPrice(price)} ₽</span>
+        </div>
+        <button class="cart-product__delete" aria-label="Удалить товар из корзины">
+        </button>
+      </article>
+    </li>
+  `
+}
+
+const deleteProducts = (productParentEl) => {
+  const id = productParentEl.querySelector('.cart-product').dataset.id
+  const currentPrice = parseInt(getPriceWithoutSpaces(productParentEl.querySelector('.cart-product__price').textContent))
+
+  document.querySelector(`.product[data-id="${id}"]`).querySelector('.product__btn').disabled = false
+  minusFullPrice(currentPrice)
+  productParentEl.remove()
+  printFullPrice()
+  printQuantity()
+}
+
+productBtnEls.forEach((el) => {
+  el.closest('.product').setAttribute('data-id', getRandomId())
+  el.addEventListener('click', (e) => {
+    const self = e.target
+    const parent = self.closest('.product')
+    const id = parent.dataset.id
+    const img = parent.querySelector('.img-switch__img img').getAttribute('src')
+    const title = parent.querySelector('.product__title').textContent
+    const priceNummber = parseInt(getPriceWithoutSpaces(parent.querySelector('.product-price__current').textContent))
+    const listEl = cartProductsListEl.querySelector('.simplebar-content')
+
+    self.disabled = true
+    plusFullPrice(priceNummber)
+    listEl.insertAdjacentHTML('afterbegin', generateCartProduct(img, title, priceNummber, id))
+    printFullPrice()
+    printQuantity()
+  })
+})
+
+cartProductsListEl.addEventListener('click', (e) => {
+  if (e.target.classList.contains('cart-product__delete')) {
+    deleteProducts(e.target.closest('.cart-content__item'))
+  }
+})
